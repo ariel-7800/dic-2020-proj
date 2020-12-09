5e764bcfd920cea727cac0a9b646cac938d1a24a
David Antabi <davidantabi@MacBook-Pro-6.local>
2020-12-09 17:14:49 -0500

    Finally

---
+++ src/components/battleship/BattleshipHome.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class BattleshipHome extends React.Component{
+
+    render () {
+        return (
+            <div>
+                <h1>Uri</h1>
+            </div>
+        )
+    }
+}
+
+export default BattleshipHome;
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,13 +1,46 @@
 import React from "react"
+import ConnectHome from "../connect4/ConnectHome";
+import BattleshipHome from "../battleship/BattleshipHome";
+
 
 class Home extends React.Component{
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentGame: null,
+        };
+    }
+    
+    launchBattleShip = (event) => {
+        event.preventDefault();
+        this.setState({
+            currentGame: "Battleship"
+        });
+    };
+
+    launchConnect4 = (event) => {
+        event.preventDefault();
+        this.setState({
+            currentGame: "Connect4"
+        });
+    };
 
     render () {
         return (
-            <div>
-                <h1>David & Uirel</h1>
-            </div>
+                <div>
+                    {
+                        this.state.currentGame === null ? 
+                            <div>
+                                <input type="submit" value="Connect4" onClick={this.launchConnect4}/>
+                                <input type="submit" value = "Battleship" onClick={this.launchBattleShip}/>
+                            </div>
+                        :
+                            this.state.currentGame === "Connect4" ? <ConnectHome/> : <BattleshipHome/>
+
+                    }
+                </div>
+            
         )
     }
 }
